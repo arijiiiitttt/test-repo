be53a7e4fccf74f806580ae365e80942db18e45e
arijiiiitttt🍉 <arijit123roy098@gmail.com>
2025-10-28 12:10:36 +0530

    portfolio-02

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [talwindcss() , react()],
+  plugins: [tailwindcss() , react()],
 })
